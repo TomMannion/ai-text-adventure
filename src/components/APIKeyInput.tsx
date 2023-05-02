bbc941ae08c4224951d2711736eec59d0b703375
Your Name <you@example.com>
2023-05-02 09:02:33 +0100

    Added link to OpenAI for User ease

--- src/components/APIKeyInput.tsx
+++ src/components/APIKeyInput.tsx
@@ -16,7 +16,7 @@ const APIKeyInput: React.FC<APIKeyInputProps> = ({ onAPIKeySubmit }) => {
 
   return (
     <div className="api-wrapper">
-      <h2>Enter your API key:</h2>
+      <h2>Enter your OpenAI API key:</h2>
       <form onSubmit={handleSubmit} className="component-container api-input">
         <input
           type="text"
@@ -27,6 +27,7 @@ const APIKeyInput: React.FC<APIKeyInputProps> = ({ onAPIKeySubmit }) => {
         <button type="submit">Submit</button>
       </form>
       <p>This is all run in the browser so the api key is private, it is used to generate your own story.</p>
+      <p>Get your API key from <a href="https://beta.openai.com/" target="_blank" rel="noopener noreferrer">OpenAI</a></p>
     </div>
   );
 };
