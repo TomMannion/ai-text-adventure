dbb2bb88833f07c0a66212cc70411716c8990937
Your Name <you@example.com>
2023-05-02 09:23:52 +0100

    fixed styling issue with GameOutput, this was to create a fixed bar at the top with stats

--- src/components/GameOutput.tsx
+++ src/components/GameOutput.tsx
@@ -13,17 +13,22 @@ const GameOutput: React.FC<GameOutputProps> = ({ output, genre, turnCount, isLoa
   console.log('GameOutput output:', output);
   return (
     <div className="game-output component-container">
-      <p>Genre: {genre}</p>
-      <p>Turns: {turnCount}</p>
-      <p>{isLoading ? 'Loading response...' : ''}</p>
-      {output.map((text, index) => (
-        <p key={index}>{text + '\n'}</p>
-      ))}
+      <div className="game-output-info">
+        <p>Genre: {genre}</p>
+        <p>Turns: {turnCount}</p>
+        <p>{isLoading ? 'Loading response...' : ''}</p>
+      </div>
+      <div className="game-output-content">
+        {output.map((text, index) => (
+          <p key={index}>{text + '\n'}</p>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default GameOutput;
+
 
 
 
